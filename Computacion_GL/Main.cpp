dc046c363623cdc8c225285789b57c5cc20d939a
Jesús Ramis Galindo <113546651+jramisDev@users.noreply.github.com>
2024-11-07 19:04:35 +0100

    Rectangulo y estrella

--- Computacion_GL/Main.cpp
+++ Computacion_GL/Main.cpp
@@ -2,10 +2,55 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+/*
+TRIANGULO
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+};
+*/
+
+/*RECTANGULO
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f,
+};
+*/
+
+/*ESTRELLA*/
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  1.0f, 0.0f,
+     0.5f, 0.5f, 0.0f,
+     -0.5f, 0.5f, 0.0f,
+     0.0f,  -1.0f, 0.0f,
+};
+
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.1f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
 void frameBuffer_size_callback(GLFWwindow* Window, int width, int height) {
-
     glViewport(0, 0, width, height);
-
 }
 
 int main(void)
@@ -35,14 +80,56 @@ int main(void)
 
     glfwSetFramebufferSizeCallback(Window, frameBuffer_size_callback);
 
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    unsigned int VBO;
+    unsigned int VAO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //solo para el Triangulo, cambia parametros para otra forma
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    //Wireframe activado
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     while (!glfwWindowShouldClose(Window)) {
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);// 3 para un triangulo, 6 para una figura hecha con dos triangulos
 
         glfwSwapBuffers(Window);
         glfwPollEvents();
-
     }
 
-    glViewport(0, 0, 800, 600);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    //glViewport(0, 0, 800, 600);
 
     glfwTerminate();
     return 0;
