a5f69ad4e221990320bb555a0a23e17f7aad9994
Jesús Ramis Galindo <113546651+jramisDev@users.noreply.github.com>
2024-12-12 09:08:00 +0100

    Incluimos librerias de GLM Matematicas

--- Computacion_GL/Main.cpp
+++ Computacion_GL/Main.cpp
@@ -5,6 +5,9 @@
 #include <GLFW/glfw3.h>
 #include <STB/stb_image.h>
 #include <string>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Shader {
 public:
